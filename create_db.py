e2340da54f7921c392e67e204c67c5b422e05dc7
Mira_Ridleton <104515741+Yana2121@users.noreply.github.com>
2023-09-25 19:16:47 +0500

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from models import User, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db.init_app(app)
 
 if __name__ == '__main__':
